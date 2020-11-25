ec2c0846d4cd55ef024055c1537d54601a81a279
Justin Hubbell <justin@jwhubbell.com>
2020-11-24 22:16:15 -0600

    compiled my first C++ program! set up main function

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,6 +5,16 @@ int main()
 { 
   std::cout << "Quickly!  The magnetic seals are failing, we are venting oxygen into space!"; 
   std::cout<< std::endl; 
-  std::cout << "Enter the correct codes to engage emergency hatches!"; 
+  std::cout << "Enter the correct codes to engage emergency hatches!";
+
+  int a = 4;
+  int b = 2; 
+  int c = 1; 
+
+  int sum = a + b + c;  
+
+  std::cout << std::endl; 
+  std::cout << sum; 
+
   return 0; 
 }
