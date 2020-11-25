50f505e67c4825d796f0d2fb2b0826a926a88c4c
Justin Hubbell <justin@jwhubbell.com>
2020-11-24 21:52:29 -0600

    created git repo for files

---
+++ TripleX.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main()
+{ 
+  std::cout << "Quickly!  The magnetic seals are failing, we are venting oxygen into space!"; 
+  std::cout<< std::endl; 
+  std::cout << "Enter the correct codes to engage emergency hatches!"; 
+  return 0; 
+}
